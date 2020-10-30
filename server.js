96af6692eb5ccff6adbe002fef70c621d52cb955
cw <illumea@gmail.com>
2020-10-30 13:59:11 +0800

    Commit 6: Add user login, register routes, middleware for user login, dashboard and logout routes

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ require('dotenv').config();
 const express = require('express')
 const methodOverride = require('method-override')
 const mongoose = require('mongoose')
+const session = require('express-session')
 const app = express();
 const port = 5000;
 
 // ====== IMPORT ======
 const productsController = require('./controllers/ProductsController');
+const usersController = require('./controllers/UsersController');
 
 // see env variables
 // console.log(process.env);
@@ -37,7 +39,19 @@ app.use(express.urlencoded({
   extended: true
 }))
 
-// ====== ROUTES ======
+// import session package
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    name: "app_session",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, maxAge: 3600000 } // 3600000ms = 3600s = 60mins, cookie expires in an hour
+  }))
+
+app.use(setUserVarMiddleware)
+
+
+// ====== PRODUCT ROUTES ======
 
 // redirect main index to products
 app.get('', productsController.redirectToMain)
@@ -68,6 +82,30 @@ app.patch('/products/:slug', productsController.updateProduct)
 app.delete('/products/:slug', productsController.deleteProduct)
 
 
+// ====== USER ON-BOARDING ROUTES ======
+// apply guestOnlyMiddleware here
+
+// user registration form route
+app.get('/users/register', guestOnlyMiddleware, usersController.showRegistrationForm)
+
+// user registration
+app.post('/users/register', guestOnlyMiddleware, usersController.register)
+
+// user login form route
+app.get('/users/login', guestOnlyMiddleware, usersController.showLoginForm)
+
+// user login route
+app.post('/users/login', guestOnlyMiddleware, usersController.login)
+
+// ====== USER-ONLY ROUTES ======
+
+// user dashboard
+// apply authenticatedMiddleware here
+app.get('/users/dashboard', authenticatedOnlyMiddleware, usersController.dashboard)
+
+// user logout
+app.post('/users/logout', authenticatedOnlyMiddleware, usersController.logout)
+
 
 // ====== LISTENER ======
 mongoose.connect( mongoURI, { useNewUrlParser: true, useUnifiedTopology: true } )
@@ -82,3 +120,42 @@ mongoose.connect( mongoURI, { useNewUrlParser: true, useUnifiedTopology: true } 
   .catch(err => {
     console.log(err)
   })
+
+
+// ====== MIDDLEWARE FOR LOGIN ======
+
+function guestOnlyMiddleware(req, res, next) {
+      // check if user is logged in
+      // If logged in, redirect back to dashboard
+      if (req.session && req.session.user) {
+          res.redirect('/users/dashboard')
+          return
+      }
+      next()
+}
+
+// for authenticated users only
+function authenticatedOnlyMiddleware(req, res, next) {
+    if(! req.session || ! req.session.user) {
+      res.redirect('/users/login')
+      return
+    }
+  
+    next()
+  
+}
+
+// logout middleware
+function setUserVarMiddleware(req, res, next) {
+    // default user template var set to null
+    res.locals.user = null
+  
+    // check if req.session.user is set
+    // if set, template user var will be set as well
+    if (req.session && req.session.user) {
+      res.locals.user = req.session.user
+    }
+  
+    next()
+  }
+
